2b8d49140901f698484baa31b2d392ad17aa73b7
jahnvi-sharma1526 <jahnvisharma1305@gmail.com>
2026-09-13 14:29:41 +0530

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: "/Safe-Her-delhi/",
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
